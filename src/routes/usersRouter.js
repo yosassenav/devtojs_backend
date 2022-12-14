d9479e74861032c9954636ca7cac5edf6aece066
jessica <jessicahercon@gmail.com>
2022-12-13 22:06:57 -0600

    changes added

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,26 +1,38 @@
-//funciones de los usecases son findByEmail(email) -> user, 
+//funciones de los usecases son findByEmail(email) -> user, OK
 // update(email, data) -> updatedUser, 
 // authenticate(email, password) -> token, 
 // create(email, password) -> user.
 //la nomenclatura que le di es NombreFuncion(atributos) -> return
 //y user y updatedUser son instancias del modelo user
 //email es un string, password también y data es un objeto literal
+//
 
 
 const {Router}=require("express");
 const routes= Router();
 const {
-    create,
-findByEmail,
+create,
+getByEmail,
 update,
 authenticate,
 }=require("../usecases/user"); //verificar ruta de user
+
+
+
+routes.get("/", async (req,res)=>{
+    const { email}= await getByEmail(email);
+    res.json({
+        ok:true,
+        payload:{user},
+    })
+});
+
 
 routes.get("/email", async(req,res)=>{
 const {email}=req.params;
 
 try {
-    const emailFound= await findByEmail(email);
+    const emailFound= await getByEmail(email);
     res.json({
         ok:true,
         payload:{emailFound},
@@ -31,3 +43,15 @@ try {
 }
 });
 
+//¿qué necesito del usuario? Email, password
+routes.post("/",async(req,res)=>{
+   const  {email, password}=req.body;
+
+   try {
+    //obtener el usuario 
+   } catch (error) {
+    const {message}=error;
+    res.status(404).json({ok:false, message});
+    
+   }
+});
